128cc84d0d22d7745a820da3c56348565f2a0e1c
Sarah Nadler <sarah.m.nadler@gmail.com>
2019-03-16 12:50:28 -0600

    initial commit and api working

--- models.js
+++ models.js
@@ -2,62 +2,29 @@
 
 const mongoose = require("mongoose");
 
-// this is our schema to represent a restaurant
-const restaurantSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  borough: { type: String, required: true },
-  cuisine: { type: String, required: true },
-  address: {
-    building: String,
-    // coord will be an array of string values
-    coord: [String],
-    street: String,
-    zipcode: String
-  },
-  // grades will be an array of objects
-  grades: [
-    {
-      date: Date,
-      grade: String,
-      score: Number
-    }
-  ]
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
+  }
 });
 
-// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
-// allow us to define properties on our object that manipulate
-// properties that are stored in the database. Here we use it
-// to generate a human readable string based on the address object
-// we're storing in Mongo.
-restaurantSchema.virtual("addressString").get(function() {
-  return `${this.address.building} ${this.address.street}`.trim();
+postSchema.virtual("authorString").get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
-// this virtual grabs the most recent grade for a restaurant.
-restaurantSchema.virtual("grade").get(function() {
-  const gradeObj =
-    this.grades.sort((a, b) => {
-      return b.date - a.date;
-    })[0] || {};
-  return gradeObj.grade;
-});
-
-// this is an *instance method* which will be available on all instances
-// of the model. This method will be used to return an object that only
-// exposes *some* of the fields we want from the underlying data
-restaurantSchema.methods.serialize = function() {
+postSchema.methods.serialize = function() {
   return {
     id: this._id,
-    name: this.name,
-    cuisine: this.cuisine,
-    borough: this.borough,
-    grade: this.grade,
-    address: this.addressString
+    title: this.title,
+    content: this.content,
+    author: this.authorString,
+    created: Date.now().toString()
   };
 };
 
-// note that all instance methods and virtual properties on our
-// schema must be defined *before* we make the call to `.model`.
-const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+const Post = mongoose.model("Post", postSchema);
 
-module.exports = { Restaurant };
+module.exports = { Post };
--- server.js
+++ server.js
@@ -3,30 +3,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-// Mongoose internally uses a promise-like object,
-// but its better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
 
-// config.js is where we control constants for entire
-// app like PORT and DATABASE_URL
 const { PORT, DATABASE_URL } = require("./config");
-const { Restaurant } = require("./models");
+const { Post } = require("./models");
 
 const app = express();
 app.use(express.json());
 
-// GET requests to /restaurants => return 10 restaurants
-app.get("/restaurants", (req, res) => {
-  Restaurant.find()
-    // we're limiting because restaurants db has > 25,000
-    // documents, and that's too much to process/return
-    .limit(10)
-    // success callback: for each restaurant we got back, we'll
-    // call the `.serialize` instance method we've created in
-    // models.js in order to only expose the data we want the API return.    
-    .then(restaurants => {
+app.get("/posts", (req, res) => {
+  Post.find()  
+    .then(posts => {
       res.json({
-        restaurants: restaurants.map(restaurant => restaurant.serialize())
+        posts: posts.map(post => post.serialize())
       });
     })
     .catch(err => {
@@ -35,21 +24,18 @@ app.get("/restaurants", (req, res) => {
     });
 });
 
-// can also request by ID
-app.get("/restaurants/:id", (req, res) => {
-  Restaurant
-    // this is a convenience method Mongoose provides for searching
-    // by the object _id property
+app.get("/posts/:id", (req, res) => {
+  Post
     .findById(req.params.id)
-    .then(restaurant => res.json(restaurant.serialize()))
+    .then(post => res.json(post.serialize()))
     .catch(err => {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
     });
 });
 
-app.post("/restaurants", (req, res) => {
-  const requiredFields = ["name", "borough", "cuisine"];
+app.post("/posts", (req, res) => {
+  const requiredFields = ["title", "content", "author"];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -59,22 +45,19 @@ app.post("/restaurants", (req, res) => {
     }
   }
 
-  Restaurant.create({
-    name: req.body.name,
-    borough: req.body.borough,
-    cuisine: req.body.cuisine,
-    grades: req.body.grades,
-    address: req.body.address
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
   })
-    .then(restaurant => res.status(201).json(restaurant.serialize()))
+    .then(post => res.status(201).json(post.serialize()))
     .catch(err => {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
     });
 });
 
-app.put("/restaurants/:id", (req, res) => {
-  // ensure that the id in the request path and the one in request body match
+app.put("/posts/:id", (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message =
       `Request path id (${req.params.id}) and request body id ` +
@@ -83,11 +66,8 @@ app.put("/restaurants/:id", (req, res) => {
     return res.status(400).json({ message: message });
   }
 
-  // we only support a subset of fields being updateable.
-  // if the user sent over any of the updatableFields, we udpate those values
-  // in document
   const toUpdate = {};
-  const updateableFields = ["name", "borough", "cuisine", "address"];
+  const updateableFields = ["title", "content", "author"];
 
   updateableFields.forEach(field => {
     if (field in req.body) {
@@ -95,30 +75,24 @@ app.put("/restaurants/:id", (req, res) => {
     }
   });
 
-  Restaurant
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+  Post
     .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-    .then(restaurant => res.status(204).end())
+    .then(post => res.status(200).json(post.serialize()))
     .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
-app.delete("/restaurants/:id", (req, res) => {
-  Restaurant.findByIdAndRemove(req.params.id)
-    .then(restaurant => res.status(204).end())
+app.delete("/posts/:id", (req, res) => {
+  Post.findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
     .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
-// catch-all endpoint if client makes request to non-existent endpoint
 app.use("*", function(req, res) {
   res.status(404).json({ message: "Not Found" });
 });
 
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, so we declare `server` here
-// and then assign a value to it in run
 let server;
 
-// this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(
@@ -141,8 +115,6 @@ function runServer(databaseUrl, port = PORT) {
   });
 }
 
-// this function closes the server, and returns a promise. we'll
-// use it in our integration tests later.
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
@@ -157,10 +129,8 @@ function closeServer() {
   });
 }
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = { app, runServer, closeServer };
+ module.exports = { app, runServer, closeServer };
